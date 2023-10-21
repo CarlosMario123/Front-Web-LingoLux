f63cfb488ba9a0c4fc02f6b8f27fb9ee7de5934e
Eduardo Trujillo <129311642+Lalo64GG@users.noreply.github.com>
2023-10-20 19:30:32 -0600

    Delete components directory

--- components/Atoms/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Button({ children }) {
-  return (
-    <button className = 'p-2 border mr-50 principal-color rounded-md trasition duration-300 ease-in-out hover:text-white' >
-        { children } //? Utilizamos Children para poner el texto del boton
-    </button>
-  )
-}
--- components/Molecules/CardsRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const CardsRoute = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
