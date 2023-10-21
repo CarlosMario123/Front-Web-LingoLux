3a936a901bef8d40e7e571af445e4ec1b431f20a
Eduardo Trujillo <129311642+Lalo64GG@users.noreply.github.com>
2023-10-20 19:47:02 -0600

    Delete components directory

--- components/Atoms/UserImg.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function UserImg({url = "https://w7.pngwing.com/pngs/524/696/png-transparent-computer-icons-user-symbol-symbol-miscellaneous-black-computer-icons.png"}) {
-  return (
-    <div className="w-10 h-10 rounded-full">
-        <img src={url} alt="" className="w-10 h-10 rounded-full"/>
-    </div>
-  )
-}
