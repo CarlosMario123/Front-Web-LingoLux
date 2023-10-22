eb03b266df8d77e19f58081d549da145a96a8df6
Lalo64GG <223257@ids.upchiapas.edu.mx>
2023-10-22 03:35:31 -0600

    Arreglos a los estilos del Componente

--- app/components/Molecules/CardsDiccionary.jsx
+++ app/components/Molecules/CardsDiccionary.jsx
@@ -1,9 +1,11 @@
 export const CardsDiccionary = ({ url, children, childrenEs }) => {
   return (
     <div className="flex h-[12rem] rounded-md shadow-lg color-basic w-[11rem]">
-      <img src={url} alt="" />
+      <div className = ' p-4'>
         <h2 className="w-full text-[1.1] font-semibold text-center text-gray-800"> {children} </h2>
-        <p className = " "> { childrenEs } </p>
+        <img src={url} alt="" />
+        <p className = "text-center text-gray-600 "> { childrenEs } </p>
+      </div>
     </div>
   )
 }
--- app/components/Organims/Dictionary.jsx
+++ app/components/Organims/Dictionary.jsx
@@ -1,33 +1,42 @@
-import { CardsDiccionary } from "../Molecules/CardsDiccionary"
+import { CardsDiccionary } from "../Molecules/CardsDiccionary";
+import  Navbar  from "../Molecules/Navbar";
 
 export const Dictionary = () => {
-
   const petsArray = [
-    { ingles: 'Dog', espanol: 'Perro' },
-    { ingles: 'Cat', espanol: 'Gato' },
-    { ingles: 'Fish', espanol: 'Pez' },
-    { ingles: 'Bird', espanol: 'Pájaro' },
-    { ingles: 'Rabbit', espanol: 'Conejo' },
-    { ingles: 'Hamster', espanol: 'Hámster' },
-    { ingles: 'Turtle', espanol: 'Tortuga' },
-    { ingles: 'Guinea Pig', espanol: 'Cobaya' },
-    { ingles: 'Snake', espanol: 'Serpiente' },
-    { ingles: 'Horse', espanol: 'Caballo' },
-    { ingles: 'Ferret', espanol: 'Hurón' },
-    { ingles: 'Parrot', espanol: 'Loro' },
-    { ingles: 'Goldfish', espanol: 'Pez Dorado' },
-  ]
+    { ingles: "Dog", espanol: "Perro" },
+    { ingles: "Cat", espanol: "Gato" },
+    { ingles: "Fish", espanol: "Pez" },
+    { ingles: "Bird", espanol: "Pájaro" },
+    { ingles: "Rabbit", espanol: "Conejo" },
+    { ingles: "Hamster", espanol: "Hámster" },
+    { ingles: "Turtle", espanol: "Tortuga" },
+    { ingles: "Guinea Pig", espanol: "Cobaya" },
+    { ingles: "Snake", espanol: "Serpiente" },
+    { ingles: "Horse", espanol: "Caballo" },
+    { ingles: "Ferret", espanol: "Hurón" },
+    { ingles: "Parrot", espanol: "Loro" },
+    { ingles: "Goldfish", espanol: "Pez Dorado" },
+    { ingles: "Turtle", espanol: "Tortuga" },
+    { ingles: "Guinea Pig", espanol: "Cobaya" },
+    { ingles: "Snake", espanol: "Serpiente" },
+    { ingles: "Horse", espanol: "Caballo" },
+    { ingles: "Ferret", espanol: "Hurón" },
+    { ingles: "Parrot", espanol: "Loro" },
+    { ingles: "Goldfish", espanol: "Pez Dorado" },
+  ];
 
   return (
-
-    <div className = ' flex flex-wrap justify-between h-auto sm:w-full gap-x-2 p' >
-      {
-          petsArray.map( (pet, index) =>(
-            <CardsDiccionary key =  { index } children = { pet.ingles } childrenEs = { pet.espanol } /> 
-          ))
-      }
+    <div>
+      <Navbar/>
+      <div className=" sm:w-full flex justify-around py-4 px-2 flex-wrap gap-x-4 gap-y-4 ">
+        {petsArray.map((word, index) => (
+          <CardsDiccionary
+            key={index}
+            children={word.ingles}
+            childrenEs={word.espanol}
+          />
+        ))}
+      </div>
     </div>
-
-  )
-}
-
+  );
+};
